e08d4d949ae34373d4b9363fc93b13453dc08cd8
Kyle <kyle@192-168-1-109.tpgi.com.au>
2022-03-13 17:23:34 +1100

    quick mobile update

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,11 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  width: 100vw;
-  height: 100vh;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  position: absolute;
   overflow: hidden;
   display: flex;
   flex-direction: column;
